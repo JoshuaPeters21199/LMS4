0dda4bcf50cc121a9a9ff04f20a55a6de78d2088
JoshuaPeters <jgp2.11.99@gmail.com>
2024-04-24 17:21:44 -0600

    Table header columns now generate using a loop.

--- src/views/TeamsPage/TeamListView/TeamsTable/TeamsTable.jsx
+++ src/views/TeamsPage/TeamListView/TeamsTable/TeamsTable.jsx
@@ -6,15 +6,17 @@ import TeamRow from './TeamRow';
 
 function TeamsTable({ teams, sortCol, sortDir, viewModel, onHandleDelete, onHandleSort, onHandleEdit }) {
     // console.log(teams)
-    // let columns = viewModel.list.columns;
+    let columns = viewModel.list.columns;
+    // console.log(columns);
     return(
         <>
             <div className="table-responsive">
                 <Table striped hover variant='dark'>
                     <thead>
                         <TeamHeaderRow 
-                            label={viewModel.list}
-                            colName={viewModel.list}
+                            columnArr={columns}
+                            // label={viewModel.list}
+                            // colName={viewModel.list}
                             sortCol={sortCol}
                             sortDir={sortDir}
                             onHandleSort={onHandleSort}
